c3361c7e9ff56bfa33459048f1f875016a656cd4
Мануэль Горенков <igorenkov54@mail.ru>
2019-04-16 03:22:57 +0700

    Add comments

--- Змейка/Змейка.c
+++ Змейка/Змейка.c
@@ -5,43 +5,44 @@
 #include <conio.h>
 #include <dos.h>
 
-#define WIDTH 60
-#define HEIGHT 20
-#define TIME 70
-#define PERSENTAGE_OF_WALLS 0
-#define PERSENTAGE_OF_APPLES 1
+#define WIDTH 60	//Ширина игрового поля
+#define HEIGHT 20	//Длина игрового поля
+#define TIME 70		//Время в мс между задержкой кадров (скорость змейки)
+#define PERSENTAGE_OF_WALLS 0	//Процент кол-ва стен от общего кол-ва клеток поля
+#define PERSENTAGE_OF_APPLES 1	//Процент кол-ва яблок от кол-ва оставшихся после заполнения стенами свободных клеток поля
 
 int width = WIDTH + 2;
 int height = HEIGHT + 2;
-char arr[HEIGHT + 2][WIDTH + 2];
+char arr[HEIGHT + 2][WIDTH + 2];	//Глобальный массив, в котором хранится вся информация о поле
 int game_over, move_x, move_y, apple_x, apple_y, wall_x, wall_y, maximum, count;
-char direct = 'v';
-
+char direct = 'v';	//Значок для головы змеи
+//Структура с координатами части тела змейки(row - номер строки, col - номер столбца)
 typedef struct place {
 	int row, col;
 } place;
-
+//Структура с информацией о змейке
 typedef struct Snake {
-	place head;
-	place body[WIDTH*HEIGHT];
-	int length;
-	int score;
+	place head;	//Координаты головы
+	place body[WIDTH*HEIGHT];	//Координаты всех частей тела змеи(0-й элемент в массиве - голова)
+	int length;	//Длина змеи
+	int score;	//Кол-во очков в игре (число съеденных яблок)
 } Snake;
-
+//Функция для помещения курсора в точку (x, y) (взял с форума)
 void setcur(int x, int y) {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 };
-
+//Просто функция с инициализацией переменных
 void start() {
 	game_over = 0;
 	count = 0;
 }
-
+//Функция инициализации змеи
 Snake* init_snake() {
-	Snake* snake = (Snake*)malloc(sizeof(Snake));
+	Snake* snake = (Snake*)malloc(sizeof(Snake));	//Выделение памяти под "змею"
+	//Координаты головы изначально - (0, 1)
 	snake->head.col = 1;
 	snake->head.row = 0;
 	snake->body[0].col = snake->head.col;
@@ -56,7 +57,7 @@ Snake* init_snake() {
 
 	return snake;
 }
-
+//Функция рандомного заполнения стенами
 void put_wall() {
 	for (int i = 0; i < PERSENTAGE_OF_WALLS * WIDTH * HEIGHT / 100; i++) {
 		do {
@@ -66,7 +67,7 @@ void put_wall() {
 		arr[wall_y][wall_x] = '#';
 	}
 }
-
+//Функция рандомного заполнения яблоками
 void put_apple() {
 	maximum = 0;
 	for (int i = 0; i < PERSENTAGE_OF_APPLES * WIDTH * HEIGHT * (100 - PERSENTAGE_OF_WALLS) / 100 / 100; i++) {
@@ -78,7 +79,7 @@ void put_apple() {
 		maximum++;
 	}
 }
-
+//Функция инициализации поля (постройка граничных стен, постройка стен внутри, заполнение яблоками)
 void init_field() {
 	for (int i = 0; i < width; i++) {
 		arr[0][i] = '#';
@@ -92,7 +93,7 @@ void init_field() {
 	put_wall();
 	put_apple();
 }
-
+//Функция стартует с точки (1, 1) и, двигаясь в порядке "вверх, вправо, вниз, влево", заполняет пустые клетки символом "*" (вместо яблок ставится символ "%" исключительно для удобства)
 void check_cell(int y, int x) {
 	if (arr[y][x] == ' ' || arr[y][x] == '\0') {
 		arr[y][x] = '*';
@@ -102,7 +103,7 @@ void check_cell(int y, int x) {
 		check_cell(y, x - 1);
 	}
 	else if (arr[y][x] == '@') {
-		count++;
+		count++;	//Счётчик доступных яблок (т.е. тех, до которых змейка может добраться)
 		arr[y][x] = '%';
 		check_cell(y - 1, x);
 		check_cell(y, x + 1);
@@ -110,7 +111,18 @@ void check_cell(int y, int x) {
 		check_cell(y, x - 1);
 	}
 }
+//Функция проверки поля на то, что до всех яблок змейка добраться и что змейка может из точки входа в лабиринт прийти в точку выхода
+void check_access() {
+	check_cell(1, 1);
+	if ((count == maximum) && (arr[1][1] == '*') && (arr[HEIGHT][WIDTH] == '*')) {
+		game_over = 0;
+	}
+	else {
+		game_over = 2;
+	}
 
+}
+//Функция очистки массива от символов "*" и "%", оставшизся после функции check_cell и замены на изначальные символы " " и "@"
 void clean() {
 	setcur(0, 0);
 	for (int i = 0; i < height; i++) {
@@ -124,47 +136,36 @@ void clean() {
 		}
 	}
 }
-
-void check_access() {
-	check_cell(1, 1);
-	if ((count == maximum) && (arr[1][1] == '*') && (arr[HEIGHT][WIDTH] == '*')) {
-		game_over = 0;
-	}
-	else {
-		game_over = 2;
-	}
-
-}
-
+//Функция чтения направления движения с клавиатуры
 void move(Snake* snake) {
-	if (_kbhit()) {
+	if (_kbhit()) {	//функция работает только при вводе символа
 		switch (_getch()) {
 		case 'w':
-			if (direct != 'v') {
+			if (direct != 'v') {	//Проверка, что змейка не пойдёт назад (т.е. сама в себя)
 				move_x = 0;
 				move_y = -1;
-				direct = '^';
+				direct = '^';	//Изменение символа головы
 			}
 			break;
 		case 's':
-			if (direct != '^') {
+			if (direct != '^') {	//Проверка, что змейка не пойдёт назад (т.е. сама в себя)
 				move_x = 0;
 				move_y = 1;
-				direct = 'v';
+				direct = 'v';	//Изменение символа головы
 			}
 			break;
 		case 'a':
-			if (direct != '>') {
+			if (direct != '>') {	//Проверка, что змейка не пойдёт назад (т.е. сама в себя)
 				move_x = -1;
 				move_y = 0;
-				direct = '<';
+				direct = '<';	//Изменение символа головы
 			}
 			break;
 		case 'd':
-			if (direct != '<') {
+			if (direct != '<') {	//Проверка, что змейка не пойдёт назад (т.е. сама в себя)
 				move_x = 1;
 				move_y = 0;
-				direct = '>';
+				direct = '>';	//Изменение символа головы
 			}
 			break;
 		default:
@@ -173,17 +174,18 @@ void move(Snake* snake) {
 		}
 	}
 }
-
+//Функция рисования символа symb в точке (x, y)
 void draw_symbol(int y, int x, char symb) {
 	setcur(x, y);
 	putchar(symb);
 }
-
+//Главная функция проверки на столкновение или поедание яблока
 void check(Snake* snake) {
+	//Столкновение
 	if (arr[snake->head.row + move_y][snake->head.col + move_x] == '#' || arr[snake->head.row + move_y][snake->head.col + move_x] == 'o') {
 		game_over = -1;
 	}
-	else if (arr[snake->head.row + move_y][snake->head.col + move_x] == '@') {
+	else if (arr[snake->head.row + move_y][snake->head.col + move_x] == '@') {	//Поедание яблока
 		arr[snake->head.row][snake->head.col] = 'o';
 		draw_symbol(snake->head.row, snake->head.col, 'o');
 		snake->head.col += move_x;
@@ -197,11 +199,11 @@ void check(Snake* snake) {
 
 		arr[snake->head.row][snake->head.col] = direct;
 		draw_symbol(snake->head.row, snake->head.col, direct);
-		//put_apple();
+
 		snake->length++;
 		snake->score++;
 	}
-	else {
+	else {	//Движение на следующую пустую клетку без яблок
 		arr[snake->head.row][snake->head.col] = 'o';
 		draw_symbol(snake->head.row, snake->head.col, 'o');
 
@@ -220,10 +222,12 @@ void check(Snake* snake) {
 		arr[snake->head.row][snake->head.col] = direct;
 		draw_symbol(snake->head.row, snake->head.col, direct);
 	}
+	//Оператор убирает символ " " в точке старта после начала игры
 	if (arr[0][1] == ' ') {
 		arr[0][1] = '#';
 		draw_symbol(0, 1, '#');
 	}
+	//Оператор стирает надпись про старт и "открывает выход из лабиринта", т.е. помещает в точку выхода символ " "
 	if (snake->head.col == 1 && snake->head.row == 1) {
 		setcur(0, height + 1);
 		printf("                                  ");
@@ -231,13 +235,13 @@ void check(Snake* snake) {
 		draw_symbol(height - 1, width - 2, ' ');
 	}
 }
-
+//Функция проверки на попадание в выход из лабиринта
 void check_victory(Snake* snake) {
 	if ((snake->head.col == (width - 2)) && (snake->head.row == (height - 1))) {
 		game_over = 1;
 	}
 }
-
+//Функция рисования всего поля
 void draw() {
 	setcur(0, 0);
 	for (int i = 0; i < height; i++) {
